9ffdd7b06d136d8c0088de3875b41cb09ec870bc
Jordan Bourbonnais <clonage@gmail.com>
2022-06-19 05:02:12 -0400

    2222

--- codebase/types/ts-form/sources/ProForm.d.ts
+++
@@ -1,8 +0,0 @@
-import { Form } from "./Form";
-import { IFormConfig, IItemConfig } from "./types";
-import { ICellConfig } from "../../ts-layout";
-export declare class ProForm extends Form {
-    constructor(container: HTMLElement | string, config: IFormConfig);
-    protected _addLayoutItem(item: IItemConfig): ICellConfig;
-    protected _initUI(container: HTMLElement | string): void;
-}
--- dhtmlx.js
+++
@@ -1,8 +0,0 @@
-const popup = new dhx.Popup({
-    css: "dhx_widget--bordered"
-});
-
-popup.attachHTML("<div style='padding: 16px; text-align: center'>Hi there, <br/> welcome to DHTMLX popup samples</div>");
-document.querySelector("#show").addEventListener("click", function () {
-    popup.show("show");
-});
